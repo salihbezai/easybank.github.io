eb1baa5f4855b504287cd693fe7212066c255e12
unknown <salihbezai98@gmail.com>
2021-10-05 15:13:32 +0100

    deleting some unuseful files

--- test.js
+++
@@ -1,5 +0,0 @@
-const navlinks = document.querySelector('.nav-links')
-const buttonn = document.getElementById('btn')
-buttonn.addEventListener('click',()=>{
-    navlinks.classList.toggle('open');
-})
